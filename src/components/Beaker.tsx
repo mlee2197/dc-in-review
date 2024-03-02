93f5b5642359ed0a026c49f520cabf94cee04d50
Matt <mlee138@binghamton.edu>
2024-03-02 11:43:18 -0500

    remove unused files

--- src/components/Beaker.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export const Beaker = () => {
-  return (
-    <div className="beaker relative w-[20%] aspect-[1/2]">
-      <Image src="/images/spy/target.svg" alt="target" fill className="object-cover scale-50" />
-    </div>
-  );
-}
