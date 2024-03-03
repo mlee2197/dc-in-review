ce23ce819368858b32efa02b97ce00931a775af2
Matt <mlee138@binghamton.edu>
2024-03-03 15:01:58 -0500

    update gondola style

--- src/components/LargeGondola.tsx
+++ src/components/LargeGondola.tsx
@@ -14,7 +14,7 @@ const LargeGondola = ({ children, title }: LargeGondolaProps) => {
         className="flex items-center justify-center w-full rounded-t-[80px] bg-white"
         style={{ boxShadow: "inset -4px -4px 5px 0px rgba(0,0,0,0.25)" }}
       >
-        <div className="title w-[85%] h-[75%] p-1 rounded-[100px] bg-black opacity-0">
+        <div className="title w-[85%] h-[75%] p-1 rounded-[100px] border-4 bg-black opacity-0">
           <div className="flex items-center justify-center w-full h-full p-2 border-4 border-white border-dotted rounded-[inherit]">
             <h2 className="text-2xl font-bold text-center text-white md:text-3xl">
               {title}
