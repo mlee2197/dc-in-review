4738babaec4fdc449781ec9983efd1192f195014
Matt <mlee138@binghamton.edu>
2024-03-10 22:44:13 -0400

    increase text size

--- src/app/snapshots/page.tsx
+++ src/app/snapshots/page.tsx
@@ -5,7 +5,7 @@ export default async function SnapshotsPage() {
   const data: any = await getImages();
   return (
     <div className="relative max-w-screen min-h-screen p-6 overflow-hidden bg-blue-400 md:p-8">
-      <h1 className="text-center text-2xl md:text-4xl">Snapshots</h1>
+      <h1 className="text-center text-4xl">Snapshots</h1>
       <div className="flex flex-wrap justify-center gap-6 mt-8">
         {data?.resources &&
           data.resources.map((image: any) => (
